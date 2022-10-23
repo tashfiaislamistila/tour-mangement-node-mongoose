2420a643863c57acc30ed7f855ab5198ee5601aa
Tashfia Islam <tashfia.cse@gmail.com>
2022-10-23 00:48:31 -0700

    make schema

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const {mongoose} = require("mongoose");
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//schema design
+const productSchema = mongoose.Schema({
+name:{
+type: String,
+required:[true,"Please provide a name for this product"],
+trim:true,
+unique: [true,"Name must be unique"],
+minLength:[3,"Name must be at least 3 character"],
+maxLEngth:[100,"name is too large"],
+},
+description:{
+    type:String,
+    required:true
+    },
+
+price:{
+type:Number,
+require:true,
+min:[0,"Price can be negative"],
+},
+quantity:{
+  type:Number,
+    required:true,
+    min:[0,"quantity can not be negative"],
+    validate:{
+        validator:(value)=>{
+            const isInteger=Number.isInteger(value);
+            if(isInteger){
+                return true
+            }else{
+                return false
+            }
+        }
+    },
+    message:"Quantity must be an integer"
+},
+image: {
+    type: String,
+    required: [true, "Please provide a image url for this product."],
+},
+
+})
+
+
+
+//routes
+// const productRoute = require('./routes/product.route')
+
+app.get("/", (req, res) => {
+  res.send("Route is working! YaY!");
+});
+
+//Posting to database
+// app.use('/api/v1/tour',productRoute)
+
+ module.exports = app;
